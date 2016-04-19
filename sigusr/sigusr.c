ee82aef560b42f7a8f7ceea5c5fcc10ab36b3258
BioRyajenka <igor-sushencev@yandex.ru>
2016-04-19 11:36:36 +0300

    defense03

--- sigusr/sigusr.c
+++ sigusr/sigusr.c
@@ -1,28 +1,45 @@
 #include <signal.h>
 #include <stdlib.h>
-#include <unistd.h>
+#include <pthread.h>
 #include <stdio.h>
+#include <unistd.h>
+
 
 void my_handler(int signal, siginfo_t* info, void* unused) {
-	printf("SIGUSR%d from %d\n", signal == SIGUSR1 ? 1 : 2, info->si_pid);
+	printf("signal %d from %d\n", signal, info->si_pid);
 	exit(0);
 }
 
 int main() {
+	printf("main thread pid %d\n", (int)getpid());
 	struct sigaction sigact;
+
 	sigact.sa_flags = SA_SIGINFO;
 	sigact.sa_sigaction = my_handler;
 
 	sigset_t set;
 	
-	if (sigemptyset(&set) != 0 || sigaddset(&set, SIGUSR1) != 0 || sigaddset(&set, SIGUSR2) != 0) {
-		fprintf(stderr, "Error changing sigaction mask\n");
+	if (sigemptyset(&set) != 0) {
+		fprintf(stderr, "Error emptying sigaction mask\n");
+		return 0;
+	}
+
+	int i;
+
+	for (i = 1; i <= 30; i++) {
+		if (i != SIGKILL && i != SIGSTOP && sigaddset(&set, i) != 0) {
+			fprintf(stderr, "Error changing sigaction mask\n");
+			return 0;
+		}
 	}
 
 	sigact.sa_mask = set;
 
-	if (sigaction(SIGUSR1, &sigact, NULL) != 0 || sigaction(SIGUSR2, &sigact, NULL) != 0) {
-		fprintf(stderr, "Error setting my_handler instead of default\n");
+	for (i = 1; i <= 30; i++) {
+		if (i != SIGKILL && i != SIGSTOP && sigaction(i, &sigact, NULL) != 0) {
+			fprintf(stderr, "Error setting my_handler instead of default\n");
+			return 0;
+		}
 	}
 	
 	sleep(10);
